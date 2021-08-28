16bf18e5f68ff3e2de8e14b34f451e44c3f7d4e6
tentacle110 <koskaryawati110@gmail.com>
2021-08-28 13:00:37 +0700

    nmm

---
+++ application/views/menu/b.php
@@ -0,0 +1,24 @@
+<div class="row d-flex justify-content-center">
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Rawat Jalan</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Rawat Inap</div>
+                </div>
+            </div>
+        </a>
+    </div>
+
+</div>
---
+++ application/views/menu/c.php
@@ -0,0 +1,404 @@
+<div class="row d-flex justify-content-center">
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Petugas</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Dokter</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Barcode Presensi</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Sidik Jari</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Jam Presensi</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Jadwal Pegawai</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Jadwal Tambahan</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Presensi Harian</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Presensi Bulanan</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Temporary Presensi</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Pengajuan Cuti</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Rekap Kehadiran</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Rekap Kehadiran 2</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Kepegawaian & Gaji</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Master Berkas Pegawai</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Berkas Kepegawaian</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Riwayat Jabatan</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Riwayat Pendidikan</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Riwayat Naik Gaji</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Kegiatan Ilmiah & Pelatihan</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Riwayat Penghargaan</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Riwayat Penelitian</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">SMS Gateway</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Jenis Cidera K3</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Penyebab Kecelakaan K3</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Jenis Luka K3</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Lokasi Kejadian K3</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Dampak Cidera K3</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Jenis Pekerjaan K3</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Bagian Tubuh K3</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Peristiwa K3</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Jenis Cidera K3 per-Tahun</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Penyebab Kecelakaan K3 per-Tahun</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Jenis Luka K3 per-Tahun</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Lokasi Kejadian K3 per-Tahun</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="<?= base_url() ?>informasi/jadwal_praktek_dokter">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/informasidokter.png" class="img-fluid" alt="..."> -->
+                    <div class="d-block mt-4">Cidera K3 per-Tahun</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Jenis Pekerjaan K3 per-Tahun</div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12 mb-30">
+        <a href="">
+            <div class="card-box pd-30 height-50-p">
+                <div class="progress-box text-center">
+                    <!-- <img src="<?= base_url() ?>./assets/img/icon/iconfinder_cleaning-door-knob-object-hygiene_5728178.png" class="img-fluid" alt="..."> -->
+
+                    <div class="d-block mt-4">Bagian Tubuh K3 per-Tahun</div>
+                </div>
+            </div>
+        </a>
+    </div>
+
+
+</div>
